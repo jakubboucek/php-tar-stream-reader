b604a5a190f63c28d8499f093ebfd100910fc60e
Jakub Bouček <pan@jakubboucek.cz>
2019-12-03 15:03:07 +0100

    parse.php - add better CLI access

--- parse.php
+++ parse.php
@@ -5,8 +5,21 @@ use Tar\FileReader;
 
 require __DIR__.'/vendor/autoload.php';
 
+if (PHP_SAPI !== 'cli') {
+    http_response_code(400);
+    echo 'ERROR: Tool is callable only from command-line' . PHP_EOL;
+    die(1);
+}
+
+// Parse & validate command arguments
+if ($argc < 2) {
+    throw new \RuntimeException(sprintf('Usage: %s <file>', basename(__FILE__)));
+}
+
+
 //Download ARES archive from http://wwwinfo.mfcr.cz/ares/ares_opendata.html.cz
-$archiveFile = __DIR__ . '/source/ares_vreo_all.tar.gz';
+//$archiveFile = __DIR__ . '/source/ares_vreo_all.tar.gz';
+$archiveFile = $argv[1];
 
 $reader = new FileReader($archiveFile);
 
